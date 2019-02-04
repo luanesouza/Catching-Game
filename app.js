f5e68af1eb23467c451ad29c2efe86e98268cd35
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-02-04 13:01:52 -0500

    one mouse created and moving randomly in the screen

--- app.js
+++ app.js
@@ -11,25 +11,23 @@ const main = document.querySelector('main');
 //create one mouse
 const createMouse = () => {
   const notRandomPosition = (mouse) => {
-      mouse.style.right = 200 + 'px';
+      mouse.style.top = -200 + 'px';
     }
-    let mouse = document.createElement('div');
-    mouse.setAttribute('class', 'mouse');
-    main.appendChild(mouse);
 };
-const mouse = document.querySelector('.mouse');
+let mouse = document.createElement('div');
+mouse.setAttribute('class', 'mouse');
+main.appendChild(mouse);
+
 
 
 // move that one mouse
 const moveMouse = (mouse) => {
-  let top = Math.random() * window.innerWidth;
-  let left = 100;
-  // let bottom = 1000;
+  let top = Math.random() * window.innerHeight;
+  let left = Math.random() * window.innerWidth;
   let right = Math.random() * window.innerWidth;
   mouse.style.top = `${top}px`;
   mouse.style.left =`${left}px`;
   mouse.style.right = `${right}px`;
-  // mouse.style.bottom = `${bottom}px`;
 };
 
 //remove mouse when hovered over
@@ -37,10 +35,9 @@ mouse.addEventListener('mouseover', () => {
   mouse.classList.remove('mouse');
 });
 
-
 let mouseMove = setInterval(() => {
         moveMouse(mouse);
-      }, 1000);
+      }, 980);
 // move the mouseCheese with the cheese
 // const moveMouseCheese = (mouseCheese) => {
 //   let top = Math.random() * window.innerHeight;
