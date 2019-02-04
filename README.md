160dbd3a0fea2c8b86eea5dd0f1717b1c42a922d
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-02-04 12:36:06 -0500

    images added on project folder

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ LET'S PLAY CATCH!
 
 The goal of the game is that you catch all the mice that are running around in your kitchen floor. You have to run against time.
 Who will be faster? You or the mice?
+
+LOOSING STATE
+You lose the game if you don't catch all the mice on time.
--- app.js
+++ app.js
@@ -1,55 +1,70 @@
 console.log('Listening');
 const body = document.querySelector('body');
 const mouseCheese = document.querySelector('.mouseCheese');
-const mice = document.querySelector('.mice');
-
+const main = document.querySelector('main');
 // const createPeople = () => {
 //   const notRandomPosition = (mouseCheese) => {
 //     mouseCheese.style.top = -200 + 'px';
 //   }
 // };
-//create other random people
-const createMice = () => {
-  const notRandomPosition = (mice) => {
-      mice.style.right = 200 + 'px';
+
+//create one mouse
+const createMouse = () => {
+  const notRandomPosition = (mouse) => {
+      mouse.style.right = 200 + 'px';
     }
-    let mice = document.createElement('div');
-    mice.setAttribute('class', 'mice');
-    body.appendChild(mice);
+    let mouse = document.createElement('div');
+    mouse.setAttribute('class', 'mouse');
+    main.appendChild(mouse);
 };
+const mouse = document.querySelector('.mouse');
 
-for (i = 0; i < 5; i++){
-  createMice();
-};
 
-//move the mouseCheese with the drug
-const moveMouseCheese = (mouseCheese) => {
-  let top = Math.random() * window.innerHeight;
-  let left = Math.random() * window.innerWidth;
-  // let right = Math.random() * window.innerWidth;
-  mouseCheese.style.top = `${top}px`;
-  mouseCheese.style.left = `${left}px`;
-  // mouseCheese.style.right = `${right}px`;
-};
-//mouseCheese with drug move randomly
-let mouseCheeseMove = setInterval(() => {
-      moveMouseCheese(mouseCheese);
-    }, 1000);
-// move other people
-const moveMice = (mice) => {
+// move that one mouse
+const moveMouse = (mouse) => {
   let top = Math.random() * window.innerWidth;
   let left = 100;
-  let right = Math.random() * window.innerHeight;
-  mice.style.top = `${top}px`;
-  mice.style.left =`${left}px`;
-  mice.style.right = `${right}px`;
+  // let bottom = 1000;
+  let right = Math.random() * window.innerWidth;
+  mouse.style.top = `${top}px`;
+  mouse.style.left =`${left}px`;
+  mouse.style.right = `${right}px`;
+  // mouse.style.bottom = `${bottom}px`;
+};
 
-  let moveMice = setInterval(() => {
-        moveMouseCheese(mice);
+//remove mouse when hovered over
+mouse.addEventListener('mouseover', () => {
+  mouse.classList.remove('mouse');
+});
+
+
+let mouseMove = setInterval(() => {
+        moveMouse(mouse);
       }, 1000);
-}
+// move the mouseCheese with the cheese
+// const moveMouseCheese = (mouseCheese) => {
+//   let top = Math.random() * window.innerHeight;
+//   let left = Math.random() * window.innerWidth;
+//   // let right = Math.random() * window.innerWidth;
+//   mouseCheese.style.top = `${top}px`;
+//   mouseCheese.style.left = `${left}px`;
+//   // mouseCheese.style.right = `${right}px`;
+// };
+// //mouseCheese with cheese move randomly
+// let mouseCheeseMove = setInterval(() => {
+//       moveMouseCheese(mouseCheese);
+//     }, 1000);
+// mouseCheese.addEventListener('mouseover', () => {
+//     mice.classList.remove('mouseCheese');
+//     if (mouseCheese.length === 0) {
+//       alert('You gained 50 points');
+//     }
+//     });
 
 
-createMice();
-moveMouseCheese(mouseCheese);
-moveMice(mice);
+
+
+
+createMouse();
+// moveMouseCheese(mouseCheese);
+moveMouse(mouse);
